8d2669718cc653be76b2f24d531531298998e5d2
seonjae <tjswo2292@naver.com>
2023-02-03 17:17:09 +0900

    feat: router 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./component/auth/signUp/SignUp";
+import SignIn from "./component/auth/signIn/SignIn";
+import GlobalStyle from "./style/globalStyle";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
