4c79d7f16fba1a72373cfa62e3e26a5e68826a5c
seonjae <tjswo2292@naver.com>
2023-02-05 15:32:14 +0900

    Refactor: 파일 이름 변경

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-undef */
 import axios from "axios";
 
-export const createInstance = axios.create({
+export const client = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
