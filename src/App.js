aa6c6d546394ef45775c346f18a296f3664fc827
seonjae <tjswo2292@naver.com>
2023-02-05 15:06:46 +0900

    feat: todo 라우터 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./component/auth/signUp/SignUp";
 import SignIn from "./component/auth/signIn/SignIn";
+import Todo from "./component/todo/Todo";
 import GlobalStyle from "./style/globalStyle";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
     </>
   );
