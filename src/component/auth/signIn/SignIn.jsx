4ce2f79c4c0410f719e25261a370f197855980b8
seonjae <tjswo2292@naver.com>
2023-02-05 15:39:30 +0900

    Add: 로그인 UI추가

--- src/component/auth/signIn/SignIn.jsx
+++ src/component/auth/signIn/SignIn.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import SignUpStyle from "../signUp/signUpStyle";
+import ShareInput from "../../common/ShareInput";
+import SubmitBtn from "../../common/SubmitBtn";
 
-const SignIn = () => {
-  return <div>로그인 페이지</div>;
+const Login = () => {
+  return (
+    <SignUpStyle.Wrapper>
+      <SignUpStyle.Container>
+        <SignUpStyle.titleBox>
+          <SignUpStyle.title>Log In</SignUpStyle.title>
+        </SignUpStyle.titleBox>
+        <SignUpStyle.Form>
+          <ShareInput placeholder="이메일을 입력해 주세요" type="text" />
+          <ShareInput placeholder="비밀번호를 입력해 주세요" type="password" />
+          <SubmitBtn text="로그인" testId="signin-button" />
+        </SignUpStyle.Form>
+      </SignUpStyle.Container>
+    </SignUpStyle.Wrapper>
+  );
 };
 
-export default SignIn;
+export default Login;
