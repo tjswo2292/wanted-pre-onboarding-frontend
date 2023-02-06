2c63bfd222b17a7e61c7a9be1a594c37b56a1cfe
seonjae <tjswo2292@naver.com>
2023-02-06 22:03:50 +0900

    add: signin 로직 추가

--- src/component/auth/signIn/SignIn.jsx
+++ src/component/auth/signIn/SignIn.jsx
@@ -1,20 +1,71 @@
-import React from "react";
+/* eslint-disable no-console */
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInApi } from "../../../api/auth";
+import { emailValidation, pwValidation } from "../../../utillity/SignUpValidation";
 import SignUpStyle from "../signUp/signUpStyle";
-import ShareInput from "../../common/ShareInput";
-import SubmitBtn from "../../common/SubmitBtn";
+import ShareInput from "../../common/shareInput";
+import SubmitBtn from "../../common/submitBtn";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [isUserInfo, setIsUserInfo] = useState(false);
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    emailValidation(userInfo.email) && pwValidation(userInfo.password)
+      ? setIsUserInfo(true)
+      : setIsUserInfo(false);
+  }, [userInfo]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signInApi(userInfo.email, userInfo.password).then(() => {
+        navigate("/todo");
+      });
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
+  const handleSignUpNavi = () => {
+    navigate("/signup");
+  };
+
+  const handleEmail = (emailData) => {
+    setUserInfo({ ...userInfo, email: emailData });
+  };
+
+  const handlePw = (pwData) => {
+    setUserInfo({ ...userInfo, password: pwData });
+  };
+
   return (
     <SignUpStyle.Wrapper>
       <SignUpStyle.Container>
         <SignUpStyle.titleBox>
-          <SignUpStyle.title>Log In</SignUpStyle.title>
+          <SignUpStyle.title>Sign In</SignUpStyle.title>
         </SignUpStyle.titleBox>
-        <SignUpStyle.Form>
-          <ShareInput placeholder="이메일을 입력해 주세요" type="text" />
-          <ShareInput placeholder="비밀번호를 입력해 주세요" type="password" />
+        <SignUpStyle.Form onSubmit={handleSubmit}>
+          <ShareInput
+            handleValue={handleEmail}
+            placeholder="이메일을 입력해 주세요"
+            type="email"
+            testId="email-input"
+          />
+          <ShareInput
+            handleValue={handlePw}
+            placeholder="비밀번호를 입력해 주세요"
+            type="password"
+            testId="password-input"
+          />
           <SubmitBtn text="로그인" testId="signin-button" />
         </SignUpStyle.Form>
+        <SignUpStyle.SignInBtn onClick={handleSignUpNavi}>회원가입</SignUpStyle.SignInBtn>
       </SignUpStyle.Container>
     </SignUpStyle.Wrapper>
   );
