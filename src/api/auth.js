1b813c1dd6080821f9576070136de7b2cfd0d2a8
seonjae <tjswo2292@naver.com>
2023-02-06 22:03:27 +0900

    add: signinapi 추가

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,3 +4,7 @@ import { client } from "./client";
 export const signUpApi = async (email, password) => {
   return await client.post("/auth/signup", { email, password });
 };
+
+export const signInApi = async (email, password) => {
+  return await client.post("/auth/signin", { email, password });
+};
