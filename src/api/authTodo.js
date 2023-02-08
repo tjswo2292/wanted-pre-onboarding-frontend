4546d18e1da0ed38b6012a8767e635e37f0bae48
seonjae <tjswo2292@naver.com>
2023-02-08 10:34:52 +0900

    add: 수정, 삭제 api 추가

--- src/api/authTodo.js
+++ src/api/authTodo.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
 import { todoInstance } from "./client";
 
 export const todoCreateApi = async (todo) => {
@@ -6,4 +8,12 @@ export const todoCreateApi = async (todo) => {
 
 export const todoGetApi = async () => {
   return await todoInstance.get("/todos");
+};
+
+export const todoUpdateApi = async (isCompleted, id, todo) => {
+  return await todoInstance.put(`/todos/${id}`, { todo, isCompleted });
+};
+
+export const todoDeleteApi = async (id) => {
+  return todoInstance.delete(`/todos/${id}`);
 };
