574820d5ea860a5c144d28bc55216cb552f89bdf
seonjae <tjswo2292@naver.com>
2023-02-08 10:36:22 +0900

    feat: redirect, 체크 확인 추가

--- src/component/todo/Todo.jsx
+++ src/component/todo/Todo.jsx
@@ -1,13 +1,26 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-console */
 import React, { useEffect, useState } from "react";
-import { todoCreateApi, todoGetApi } from "../../api/authTodo";
+import { useNavigate } from "react-router-dom";
+import { todoCreateApi, todoGetApi, todoUpdateApi, todoDeleteApi } from "../../api/authTodo";
 import T from "./todoStyle";
 
 const Todo = () => {
+  const navigate = useNavigate();
   const [todoContent, setTodoContent] = useState("");
   const [todoList, setTodoList] = useState([]);
 
-  const handleAddTodo = async () => {
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/signin");
+    }
+  }, []);
+
+  useEffect(() => {
+    getTodoList();
+  }, []);
+
+  const handleCreateTodo = async () => {
     try {
       await todoCreateApi(todoContent).then((res) => {
         setTodoList([...todoList, res.data]);
@@ -17,23 +30,39 @@ const Todo = () => {
     }
   };
 
-  const handleTodoInput = (e) => {
-    const { value } = e.target;
-    setTodoContent(value);
+  const getTodoList = async () => {
+    try {
+      await todoGetApi().then((res) => {
+        setTodoList(res.data);
+      });
+    } catch (err) {
+      console.log(`todoList 가져오기 Err : ${err}`);
+    }
   };
 
-  useEffect(() => {
-    const getTodoList = async () => {
-      try {
-        await todoGetApi().then((res) => {
-          setTodoList(res.data);
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getTodoList();
-  }, []);
+  const handleChecked = async (isComplete, id, todo) => {
+    try {
+      await todoUpdateApi(isComplete, id, todo).then((res) => {
+        getTodoList();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDeleteTodo = async (id) => {
+    try {
+      await todoDeleteApi(id).then(() => {
+        getTodoList();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleTodoInput = (e) => {
+    setTodoContent(e.target.value);
+  };
 
   return (
     <T.Wrapper>
@@ -49,19 +78,25 @@ const Todo = () => {
               placeholder="Todo"
               data-testid="new-todo-input"
             />
-            <T.addBtn onClick={handleAddTodo} data-testid="new-todo-add-button">
+            <T.addBtn onClick={handleCreateTodo} data-testid="new-todo-add-button">
               추 가
             </T.addBtn>
           </T.todoAddBox>
           <T.listBox>
             <T.list>
-              {todoList.map(({ id, todo }) => (
+              {todoList.map(({ id, todo, isCompleted }) => (
                 <T.listItem key={id}>
-                  <T.checkBox type="checkbox" />
-                  <T.contents>{todo}</T.contents>
+                  <T.checkBox
+                    onChange={(e) => handleChecked(e.target.checked, id, todo)}
+                    type="checkbox"
+                    checked={isCompleted}
+                  />
+                  <T.contents>{isCompleted ? <s>{todo}</s> : todo}</T.contents>
                   <T.todoItemBtnBox>
                     <T.updateBtn data-testid="modify-button">수 정</T.updateBtn>
-                    <T.deleteBtn data-testid="delete-button">삭 제</T.deleteBtn>
+                    <T.deleteBtn onClick={() => handleDeleteTodo(id)} data-testid="delete-button">
+                      삭 제
+                    </T.deleteBtn>
                   </T.todoItemBtnBox>
                 </T.listItem>
               ))}
@@ -74,6 +109,3 @@ const Todo = () => {
 };
 
 export default Todo;
-
-// default : 수정하기, 삭제하기
-// 수정하기 버튼 클릭 : 등록하기, 삭제하기
