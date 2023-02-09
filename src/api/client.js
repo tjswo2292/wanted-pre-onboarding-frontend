7d446c544757e0d0e736756151a9bb457067e418
seonjae <tjswo2292@naver.com>
2023-02-09 14:14:35 +0900

    add: proxy 설정

--- src/api/client.js
+++ src/api/client.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
+const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
+
 export const client = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `${PROXY}${process.env.REACT_APP_API_URL}`,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export const todoInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `${PROXY}${process.env.REACT_APP_API_URL}`,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
     "Content-Type": "application/json",
