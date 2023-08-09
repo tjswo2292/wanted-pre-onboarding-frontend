f0865cc0d1fd9b7ca70229371576eedcb1a65a0e
seonjae <tjswo2292@naver.com>
2023-08-09 12:55:39 +0900

    feat: signup 추가

---
+++ src/page/signup/index.js
@@ -0,0 +1,9 @@
+import UserForm from '../../components/Form';
+
+import { PATH } from '../../api/core/constants';
+
+const SignUp = () => {
+  return <UserForm title="회원가입" apiPath={PATH.AUTH_SIGNUP} />;
+};
+
+export default SignUp;
